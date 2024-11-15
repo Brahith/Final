f1eb00fe427747dbd492bdcb10f2a42dd17d6f72
Brahith <brahithp@gmail.com>
2024-11-15 10:46:59 -0500

    Added in a login page for the user to use

--- config/app.js
+++ config/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
-const path = require('path');
-const taskRoutes = require('../routes/taskRoutes');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-const app = express();
+let app = express();
+
+let taskRoutes = require('../routes/taskRoutes');
+
+let cors = require('cors')
+// Create user model instance
+let userModel = require('../model/User');
+let User = userModel.User;
 
 // Middleware to parse URL-encoded data (for form submissions)
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +20,41 @@ app.use(express.json());
 // Set EJS as the view engine
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
+let session = require('express-session')
+let passport = require('passport')
+let passportLocal = require('passport-local')
+let localStrategy = passportLocal.Strategy;
+let flash = require('connect-flash');
+
+// Getting-started.js
+const mongoose = require('mongoose');
+let DB = require ('./db')
+// point mongoose to the DB URI
+mongoose.connect (DB.URI);
+let mongoDB = mongoose.connection;
+mongoDB.on('error',console.error.bind(console,'Connection Error'));
+mongoDB.once('open',()=>{
+    consolde.log("Connected with the MongoDB")
+});
+mongoose.connect(DB.URI,{useNewURIParser:true,useUnifiedTopology:true})
+
+// set up express-session
+app.use(session({
+    secret:"SomeSecret",
+    saveUninitialized: false,
+    resave:false
+}))
+
+// Initialize the flash
+app.use(flash());
+
+// Serealize and Deserealize the user the information
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Static files
 app.use('/Content', express.static(path.join(__dirname, '../public/Content')));
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const taskController = require('../controllers/taskController'); // Make sure the path is correct
+const taskController = require('../controllers/taskController'); // Ensure this path is correct
+const passport = require('passport');
+const DB = require('../config/db');
+let userModel = require('../model/User');
+let User = userModel.User;
 
 // Route to render all tasks
 router.get('/', taskController.getAllTasks);
@@ -19,5 +23,87 @@ router.post('/edit/:id', taskController.updateTask);
 
 // Route to handle task deletion
 router.post('/delete/:id', taskController.deleteTask);
+
+// Route to render login page
+router.get('/login', function (req, res, next) {
+    if (!req.user) {
+        res.render('auth/login', {
+            title: 'Login',
+            message: req.flash('loginMessage'),
+            displayName: req.user ? req.user.displayName : ''
+        });
+    } else {
+        return res.redirect('/');
+    }
+});
+
+// Route to handle login form submission
+router.post('/login', function (req, res, next) {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            req.flash('loginMessage', 'Authentication Error');
+            return res.redirect('/login');
+        }
+        req.login(user, (err) => {
+            if (err) {
+                return next(err);
+            }
+            return res.redirect('/Task');
+        });
+    })(req, res, next);
+});
+
+// Route to render register page
+router.get('/register', function (req, res, next) {
+    if (!req.user) {
+        res.render('auth/register', {
+            title: 'Register',
+            message: req.flash('registerMessage'),
+            displayName: req.user ? req.user.displayName : ''
+        });
+    } else {
+        return res.redirect('/');
+    }
+});
+
+// Route to handle user registration
+router.post('/register', function (req, res, next) {
+    let newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        displayName: req.body.displayName
+    });
+
+    User.register(newUser, req.body.password, (err) => {
+        if (err) {
+            console.log("Error: Inserting the new User");
+            if (err.name === "UserExistsError") {
+                req.flash('registerMessage', 'Registration Error: User already exists');
+            }
+            return res.render('auth/register', {
+                title: 'Register',
+                message: req.flash('registerMessage'),
+                displayName: req.user ? req.user.displayName : ''
+            });
+        } else {
+            return passport.authenticate('local')(req, res, () => {
+                res.redirect('/Task');
+            });
+        }
+    });
+});
+
+// Route to handle logout
+router.get('/logout', function (req, res, next) {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
+});
 
 module.exports = router;
