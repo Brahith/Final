729b6ab86bde41bb56b76de55080e9cef59241c8
Brahith <brahithp@gmail.com>
2024-11-25 22:55:24 -0500

    Used gitnore to hide the databases username and password to prevent unwanted users to get in.

--- config/db.js
+++ config/db.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
+  const mongoURI = process.env.MONGO_URI;
+
+  if (!mongoURI) {
+    console.error('Error: MONGO_URI is not defined in .env');
+    process.exit(1);  // Exit the application if the connection URI is missing
+  }
+
   try {
-    await mongoose.connect('mongodb+srv://ritikgautam:whX8Loa7LsKY85e3@cluster0.c6ra0.mongodb.net/ExpenseTracker');
+    await mongoose.connect(mongoURI);
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error);
-    process.exit(1);
+    process.exit(1);  // Exit the application in case of connection failure
   }
 };
 
