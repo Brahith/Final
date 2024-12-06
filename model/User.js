85fb6ce204fec82e1ed7f20926f21ecafe51ce4b
Brahith <brahithp@gmail.com>
2024-12-05 21:24:36 -0500

    More changes

--- model/User.js
+++ model/User.js
@@ -1,13 +1,6 @@
 let mongoose = require('mongoose');
 let passportLocalMongoose = require('passport-local-mongoose');
 let User = mongoose.Schema({
-    username:
-    {
-        type:String,
-        default:"",
-        trim:true,
-        required:'Username is required'
-    },
     /*password:
     {
         type:String,
